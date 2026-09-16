6aa4774245b69941e6f7dc0e91cbe95fc174bf71
Maksim-Osipov <max26012007@gmail.com>
2026-09-17 01:08:25 +0300

    Сделана п1.3.7

--- 134-led-module/main.c
+++ 134-led-module/main.c
@@ -3,6 +3,7 @@
 #include "hardware/gpio.h"
 #include "led.h"
 #include "log.h"
+#include "device.h"
 
 const uint BUTTON_PIN = 15;
 const uint DEBOUNCE_MS = 20;
@@ -30,7 +31,10 @@ void handle_command(int command)
     {
         log_version();
     }
-
+    else if (command == 'i')
+    {
+        device_info();
+    }
     else
     {
         LOG_ERR("unknown command: %c\n", command);
